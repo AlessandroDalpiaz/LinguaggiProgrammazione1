717450ecddda8ea38f682fb41e13ea8db6327d3a
AlessandroDalpiaz <alessandro.dalpiaz@gmail.com>
2019-05-27 17:44:28 +0200

    Es 2

--- Puntatori/main.cpp
+++ Puntatori/main.cpp
@@ -5,6 +5,9 @@ int stampa(int a);
 int stampa1(int *a);
 int stampa2(int *b);
 
+void somma_uno(int x);
+void aggiungi_uno(int * x);
+
 int main(int argc, char** argv) {
 	int a=4;
 	printf("(main) a %d\n",a);
@@ -14,8 +17,27 @@ int main(int argc, char** argv) {
 	printf("(main) a %d\n",a);
 	stampa2(&a);
 	printf("(main) a %d\n",a);
+	printf("__________________________________________________\n");
+	
+	a = 12;
+	printf("var a - Val: %d – Ind: %d (0x%0x)\n", a, &a, &a);
+	somma_uno(a);
+	printf("var a - Val: %d – Ind: %d (0x%0x)\n", a, &a, &a);
+	aggiungi_uno(&a);
+	printf("var a - Val: %d – Ind: %d (0x%0x)\n", a, &a, &a);
+	
+	
 	return 0;
 }
+void somma_uno(int x) {
+x += 1;
+printf("\nValore var x: %d\n", x);
+}
+void aggiungi_uno(int * x) {
+*x += 1;
+printf("\nValore var x: %d\n", *x);
+}
+
 int stampa(int a){
 	a=a+1;
 	printf("(stampa) %d\n",a);
