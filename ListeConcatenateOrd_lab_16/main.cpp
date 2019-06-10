244f0c30c6c9a1e59b260cb44645fd54ca3c8c43
AlessandroDalpiaz <alessandro.dalpiaz@gmail.com>
2019-06-10 14:47:58 +0200

    add insert_first & insert_last

--- ListeConcatenateOrd_lab_16/main.cpp
+++ ListeConcatenateOrd_lab_16/main.cpp
@@ -2,9 +2,24 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 typedef struct Tdato{
-	// Add dati
-	// add costruttori, distruttore, stampa
-}Tdato;
+	int index;
+	float value;
+	
+	Tdato(){
+		index=0;
+		value=0.0;
+	}
+	Tdato(int i,float v){
+		index=i;
+		value=v;
+	}
+	~Tdato(){}
+	
+	void stampa(){
+		printf("[ %d - %f ]\n",index,value);
+	}
+	
+	}Tdato;
 typedef struct Tnodo{
 	Tdato dato;
 	Tnodo* next;
@@ -22,7 +37,9 @@ typedef struct Tnodo{
 		next = n;
 	}
 	~Tnodo(){}	
-	void stampa()const{ dato.Stampa(); }
+	void stampa(){ 
+		dato.stampa();
+	}
 }Tnodo;
 
 typedef Tdato Dato;
@@ -37,19 +54,45 @@ void stampa (Nodoptr s);
 int lung (Nodoptr s);
 //inserimento in coda alla lista
 Nodoptr insert_last (Nodoptr s, Dato d);
-
 int main(int argc, char** argv) {
-	Dato d;
+	Dato d = Dato();
 	Nodoptr x = NULL;
+	
 	d.index = 2; d.value = 10.4;
 	x= insert_first (x,d);
 	stampa(x);
+	
 	d.index = 3; d.value = -1.5;
 	x = insert_last (x,d);
 	stampa(x);
+	
 	d.index = 4; d.value = 0.0;
 	x = insert_first (x,d);
 	stampa(x);
-	cout << "numero elementi in lista:" << x.lung() << endl;
+	
 	return 0;
+}
+
+Nodoptr insert_first (Nodoptr s, Dato d) {
+	if(s==NULL){
+		return new Nodo(d);
+	}
+	return new Nodo(d,s);		
+}
+void stampa (Nodoptr s){
+	s->stampa();
+}
+Nodoptr insert_last (Nodoptr s, Dato d){
+	if(s==NULL){
+		 
+		return new Nodo(d,s);
+	}
+	Nodo *q=s;
+	while (q->next!=NULL){ //cerco il puntatore prossimo disponibile, ovvero =NULL
+		q=q->next;		   // come se fosse i++; ma va a puntare il prossimo dato
+		printf("aa");		
+	}
+	//sovrascrivo a quello nullo
+	q->next= new Nodo(d);
+	return s;
 }
