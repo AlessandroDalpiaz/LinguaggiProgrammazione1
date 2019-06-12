975324880e2f3d96a6297b65d53cb75543b60898
AlessandroDalpiaz <alessandro.dalpiaz@gmail.com>
2019-06-12 10:16:06 +0200

    FIFO_lab_17

--- FIFO_Lab_17/main.cpp
+++ FIFO_Lab_17/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
+#include <ctime>
 #define MAX 20
-
+#define K 5
+#define N 3
 using namespace std;
 
 typedef char stringa[MAX];
@@ -21,7 +23,9 @@ typedef struct Tdato{
 		eta=e;
 	}
 	void stampa(){
-		cout << "nome:"<<nome<<" cognome:"<<cognome<<" eta:"<<eta <<endl;
+		printf("\tnome: %s\n",nome);
+		printf("\tcognome: %s\n",cognome);
+		printf("\teta: %d\n",eta);
 	}
 	bool compare(Tdato d){
 		if ((strcmp(nome,d.nome)==0) && (strcmp(cognome,d.cognome)==0) && (eta==d.eta)) {
@@ -41,13 +45,38 @@ typedef struct Tnodo {
 		dato = x;
 		next = n;
 	}
+	void stampa(){ 
+		dato.stampa(); 
+		printf("\t%d",&dato);
+		printf("\t%d\n",next);
+	}
 }Tnodo;
 typedef struct TipoCoda{
-	Tnodo *head;
-	Tnodo *tail;
+	Tnodo* head;	// puntatore al primo elemento inserito
+					// punta all'elemento che si dovra' leggere con get
+	Tnodo* tail;  	// puntatore ultimo elemento inserito
+					// put deve inserire nuovo elemento prima di questo            
 	TipoCoda(){
 		head=NULL;
 		tail=NULL;
+	}
+	void stampa(){
+		Tnodo *s = head;
+		while (s!=NULL){ 
+			s->stampa(); 
+			s= s->next; 
+		}
+		cout << endl;  
+	}
+	~TipoCoda(){
+		cout << "\ndistruttore\n";
+		Tnodo* tmp=head;
+		while(head!=NULL){
+			head = head->next;
+			delete tmp;
+			tmp = head;
+		}
+		tail = NULL;
 	}
 }TipoCoda;
 typedef struct TipoCoda Coda;
@@ -57,10 +86,56 @@ typedef struct TipoCoda *CodaPtr;
 CodaPtr put (CodaPtr p, Tdato d);
 //estrae l’elemento in testa alla lista
 Tdato get (CodaPtr p);
-
+int casuale(int min,int max);
+void stampa(CodaPtr p[2]);
 int main(int argc, char** argv) {
-	
+	//creo array di puntatori
+	CodaPtr p[N];
+    Tdato d;
+    int i, t,  pos;
+	//creo N code vuote!
+    for (i=0;i<N;i++) { 
+        p[i] = new TipoCoda(); 
+    }
+    
+        srand(time(0));
+    
+	for(t=0 ; t<K ; t++) { 
+		fflush(stdin);
+       	system("CLS");
+       	printf("nome=");    
+		// richiesto
+		// cin >> d.nome;
+		// per velocizzare test
+		d.nome[0] = casuale('A', 'Z');
+		d.nome[1] = casuale('a', 'z');
+		d.nome[2] = casuale('a', 'z');
+		d.nome[3] = '\0';
+		
+       	printf("cognome=");
+		d.cognome[0] = casuale('A', 'Z');
+		d.cognome[1] = casuale('a', 'z');
+		d.cognome[2] = casuale('a', 'z');
+		d.cognome[3] = '\0';
+		
+       	d.eta= casuale(1,50);
+		
+		//scelgo una coda in modo casuale per inserire il dato
+       	pos=casuale(0,2);
+       	put(p[pos], d);   //invocazione della funzione put
+	}
+	printf("\nUNO \n");
+    p[0]->stampa();
+    printf("DUE \n");
+    p[1]->stampa();
+    printf("TRE \n");
+    p[2]->stampa();
+    
+    
 	return 0;
+}
+int casuale(int min ,int max){
+	return rand()%(max-min+1)+min;
 }
 //inserisce x in coda
 CodaPtr put (CodaPtr p, Tdato d){
@@ -76,6 +151,18 @@ CodaPtr put (CodaPtr p, Tdato d){
 	p=n;
 	return p;
 }
+
+//da fixare
+/*void stampa(CodaPtr p[2]){
+	CodaPtr q=p;//???
+	for(int i=0;i<=2;i++){
+		while(q[i]->head==q[i]->tail){
+			q[i]->head->dato.stampa();
+			q[i]=q[i]->head;
+		}
+	}
+}
+*/
 //estrae l’elemento in testa alla lista
 Tdato get (CodaPtr p){
 	Tdato d; //invoco costruttore di default
